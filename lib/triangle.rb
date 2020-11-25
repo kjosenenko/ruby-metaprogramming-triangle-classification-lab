08018234ec5faa3894bbee07161ca1a018a681a9
KJ Osenenko <karljosenenko@gmail.com>
2020-11-25 11:59:46 -0700

    commit tree b3784a7e5a4f5ec937b389e476bd0b5ba2f05bb1

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if ((a && b && c) > 0) && (a + b > c) && (a + c > b) && (b + c > a)
+      if ((a == b) && (a == c) && (b == c))
+        :equilateral
+      elsif ((a == b) || (a == c) || (b == c))
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
+
 end
